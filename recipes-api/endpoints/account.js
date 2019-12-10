2a78db3b38fef6bd7ef5a93e9e0ed3d2f173fb0b
Mark <mark.j@prodege.com>
2019-12-10 09:54:40 -0700

    Return CORS header from API

--- recipes-api/endpoints/account.js
+++ recipes-api/endpoints/account.js
@@ -5,11 +5,8 @@ let DynamoDb = require('../utils/dynamo-db-utils');
 module.exports = (() => {
   async function login(event) {
     let request = JSON.parse(event.body);
-    console.log('Request:', request);
     let account = await DynamoDb.getAccount(request.email);
-    console.log('Account:', account);
     if (account && account.password === request.password) {
-      console.log('Success');
       return {
         statusCode: 200,
         body: JSON.stringify({
@@ -18,8 +15,10 @@ module.exports = (() => {
       }
     }
     else {
-      console.log('Error');
       return {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
         statusCode: 200,
         body: JSON.stringify({
           error: 'Email or Password incorrect'
